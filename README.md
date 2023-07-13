e3242763ad16721a6e4b96ae0c2f5263ac3980b5
gneginskiy <gneginskiy@mail.ru>
2023-07-13 16:16:02 +0400

    initial commit

--- README.md
+++
@@ -1,3 +0,0 @@
-# Otus Kafka
-
-Материалы для курса [Apache Kafka](https://otus.ru/lessons/kafka/)
--- example/src/main/java/Ex4Consumer.java
+++ example/src/main/java/Ex4Consumer.java
@@ -1,17 +1,15 @@
 import java.time.Duration;
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
-import org.apache.kafka.clients.producer.KafkaProducer;
-import org.apache.kafka.clients.producer.ProducerRecord;
 
 public class Ex4Consumer {
 
     public static void main(String[] args) {
         var groupId = args.length > 0 ? args[0] : "java-app";
-
         var topics = args.length > 1 ?
             new ArrayList<>(List.of(args).subList(1, args.length))
             : List.of("topic1");
@@ -25,11 +23,9 @@ public class Ex4Consumer {
 
         while (true) {
             var result = consumer.poll(Duration.ofSeconds(10));
-
             Utils.log.info("Read {}", result.count());
-
             for (var record : result) {
-                Utils.log.warn("Message {}.{}: {} -> {}", record.topic(), record.partition(),
+                Utils.log.info("Message {}.{}: {} -> {}", record.topic(), record.partition(),
                     record.key(), record.value());
             }
         }
